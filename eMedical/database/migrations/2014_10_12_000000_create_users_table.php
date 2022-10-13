99f3d69337bb7bebfca013369f3966ac6a6c5312
crisfj00 <crisfj00@correo.ugr.es>
2022-10-13 17:12:16 +0200

    Database structure created

--- eMedical/database/migrations/2014_10_12_000000_create_users_table.php
+++ eMedical/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,14 +14,36 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email')->unique()->primary();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
+
+        Schema::create('patients', function (Blueprint $table) {
+            $table->string('email')->unique();
+            $table->foreign('email')->references('email')->on('users');
+            $table->string('id_number',10)->unique(); #12345678Z ó Z12345678A
+            $table->string('healthcara_number',10)->unique();
+            $table->dateTime('birthday');
+            $table->string('occupation');
+            $table->string('address');
+            $table->string('phone_number',20);
+            $table->timestamps();
+        });
+
+        Schema::create('doctors', function (Blueprint $table) {
+            $table->string('id',9)->unique(); 
+            $table->string('email')->unique();
+            $table->foreign('email')->references('email')->on('users');
+            $table->string('specialty');
+            $table->timestamps();
+        });
+
+
+
     }
 
     /**
@@ -31,6 +53,9 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
+        Schema::dropIfExists('prescriptions');
+        Schema::dropIfExists('doctors');
+        Schema::dropIfExists('patients');
         Schema::dropIfExists('users');
     }
 }
---
+++ eMedical/database/migrations/2022_10_13_143306_create_prescriptions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePrescriptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prescriptions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('patient_id',10);
+            $table->foreign('patient_id')->references('id_number')->on('patients');
+            $table->string('doctor_id',9);
+            $table->foreign('doctor_id')->references('id')->on('doctors');
+            $table->text('consultation');
+            $table->text('diagnosis');
+            $table->boolean('state')->default(0); //0 if not responded 1 if yes
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prescriptions');
+    }
+}
