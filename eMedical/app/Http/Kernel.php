9057c95af4a20645b03c294fd632e3576cb79019
crisfj00 <crisfj00@correo.ugr.es>
2022-11-22 19:07:44 +0100

    Patient deleted after 24h implemented

--- eMedical/app/Http/Kernel.php
+++ eMedical/app/Http/Kernel.php
@@ -74,26 +74,13 @@ class Kernel extends HttpKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->call(function () {
-            /*$from = Carbon::now()->subHours(1);
-            $to = Carbon::now();
-            $unverified_users=User::where('role_id',1)->whereNull('email_verified_at')->whereNotBetween('created_at', [$from, $to]);
-            foreach($unverified_users as $user){
-                $pat=Patient::where('email',$user->email)->get()->first()->delete();
-                $user->delete();
-            }*/
+
             $users=User::whereNull('email_verified_at')->get();
             foreach($users as $user)
                 $pat=Patient::where('email',$user->email)->delete();
 
-                $users=User::whereNull('email_verified_at')->delete();
+            $users=User::whereNull('email_verified_at')->delete();
+        })->daily();
 
-            info('eccomi');
-
-        })->everyMinute();
-
-        $schedule->call(function(){
-            info('eccomi');
-
-        })->everyMinute();
     }
 }
--- eMedical/vendor/laravel/framework/src/Illuminate/Auth/Notifications/ResetPassword.php
+++ eMedical/vendor/laravel/framework/src/Illuminate/Auth/Notifications/ResetPassword.php
@@ -75,11 +75,11 @@ class ResetPassword extends Notification
     protected function buildMailMessage($url)
     {
         return (new MailMessage)
-            ->subject(Lang::get('Reset Password Notification'))
-            ->line(Lang::get('You are receiving this email because we received a password reset request for your account.'))
-            ->action(Lang::get('Reset Password'), $url)
-            ->line(Lang::get('This password reset link will expire in :count minutes.', ['count' => config('auth.passwords.'.config('auth.defaults.passwords').'.expire')]))
-            ->line(Lang::get('If you did not request a password reset, no further action is required.'));
+            ->subject(Lang::get('Welcome to eMedical'))
+            ->line(Lang::get('You are receiving this email because we registered your account in our service.'))
+            ->action(Lang::get('Log In'), $url)
+            ->line(Lang::get('This link will expire in :count hours.', ['count' => config('auth.passwords.'.config('auth.defaults.passwords').'.expire')/60]))
+            ->line(Lang::get('If you did not replied on time, please contact us back on support@emedical.com.'));
     }
 
     /**
